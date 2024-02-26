6f76e59ac64305a7ca4b798383544dbaccba9d9f
vickanS <149102936+vickanS@users.noreply.github.com>
2024-02-26 15:11:34 +0100

    wedding page content

---
+++ index.js
@@ -0,0 +1,36 @@
+function initMap() {
+    var location = {lat: 56.62612362203652, lng: 16.2359255381287};
+  
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: location
+    });
+  
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+  
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer({
+      map: map
+    });
+  
+    document.getElementById('directions-form').addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      var start = document.getElementById('start').value;
+  
+      directionsService.route({
+        origin: start,
+        destination: location,
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsRenderer.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    });
+  }

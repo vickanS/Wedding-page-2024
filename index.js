f13b9c4fa6ac2ba29370f55d7b11beab7dac62e7
vickanS <149102936+vickanS@users.noreply.github.com>
2024-03-03 18:22:48 +0100

    changed the coordinates for the marker on the map

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function initMap() {
   var mapElement = document.querySelector('.map');
   
   var map = new google.maps.Map(mapElement, {
-    center: {lat: 56.64165, lng: 16.22625},
+    center: {lat: 56.64241, lng: 16.23861},
     zoom: 8
   });
 

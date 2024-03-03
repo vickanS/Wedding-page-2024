2d4c8af66daeeb92dda6e6c9979c3027dc9f5dc5
vickanS <149102936+vickanS@users.noreply.github.com>
2024-03-03 12:02:10 +0100

    Added a google maps API

---
+++ index.js
@@ -0,0 +1,13 @@
+function initMap() {
+  var mapElement = document.querySelector('.map');
+  
+  var map = new google.maps.Map(mapElement, {
+    center: {lat: 56.64165, lng: 16.22625},
+    zoom: 8
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 56.64165, lng: 16.22625},
+    map: map
+  });
+}
